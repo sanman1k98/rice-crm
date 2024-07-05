79034ca8eaa585ba725cd52a5491723b59d88542
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-07-05 18:09:43 -0400

    feat: use `@tailwindcss/forms` plugin

--- packages/mvp/fullstack-astro/tailwind.config.ts
+++ packages/mvp/fullstack-astro/tailwind.config.ts
@@ -1,7 +1,11 @@
 import type { Config } from "tailwindcss";
+import forms from "@tailwindcss/forms";
 
 const config: Config = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  plugins: [
+    forms(),
+  ]
 };
 
 export default config;
