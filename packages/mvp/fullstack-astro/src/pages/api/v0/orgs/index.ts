ba3713a863f6930dd75e929898d851e35e82d44c
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-07-08 18:55:58 -0400

    chore(api): scaffold routes for "Organizations"

---
+++ packages/mvp/fullstack-astro/src/pages/api/v0/orgs/index.ts
@@ -0,0 +1,17 @@
+import type { APIRoute } from "astro";
+
+/**
+ * Create an organization for the authenticated user.
+ */
+export const POST: APIRoute = async () => {
+  const data = { status: 501, statusText: "Not Implemented" };
+  return Response.json(data, data);
+}
+
+/**
+ * Get a list of organizations for the authenticated user.
+ */
+export const GET: APIRoute = async () => {
+  const data = { status: 501, statusText: "Not Implemented" };
+  return Response.json(data, data);
+}
