fcf58f20297f7d1747ad9606c1b5a29a6fcd4538
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-07-08 08:06:49 -0400

    docs(db): add a bunch of TODOs to db config
    These are just fresh thoughts and notes that I needed to put somewhere.
    We should be using GitHub issues and proper documentation regarding
    architecture and database schemas at somepoint.

--- packages/mvp/fullstack-astro/db/config.ts
+++ packages/mvp/fullstack-astro/db/config.ts
@@ -6,11 +6,27 @@ import { column, defineDb, defineTable } from "astro:db";
 const User = defineTable({
   columns: {
     id: column.text({ primaryKey: true }),
+    // TODO: Consider supporting email-based auth.
+    // https://lucia-auth.com/guides/email-and-password/
     username: column.text({ unique: true }),
+    // FIXME: Deprecate username-based auth.
+    // Since this is just an MVP, its fine for now.
     password_hash: column.text(),
+    // TODO: Support multiple OAuth providers.
+    // https://arcticjs.dev
+    //
+    // TODO: Define "memberships" for users.
+    // A single user should can be "Members" of one or more "Organizations".
   }
 });
 
+/**
+ * Used by Lucia to keep track of user sessions.
+ *
+ * @see https://lucia-auth.com/database/
+ * @see https://lucia-auth.com/reference/main/Adapter
+ * @see https://github.com/pilcrowOnPaper/lucia-adapter-astrodb
+ */
 const Session = defineTable({
   columns: {
     id: column.text({ primaryKey: true }),
@@ -20,6 +36,37 @@ const Session = defineTable({
     })
   }
 });
+
+// TODO: Integrations?? How should we do integrations?
+// What level should integrations be at:
+// - the org level?
+// - the member level?
+// - the user level?
+
+// TODO: Define "Organization" table.
+// - "Organization" generally means a business, but can be something else like
+//   a nonprofit.
+// - an org can be created by a "User"
+// - an org can have multiple "Accounts" that it must keep track of
+// - an org must have at least one "Member"
+// - a "Member" of an org can be an employee
+// - a "Member" 
+
+// TODO: Define "Member" (or "Membership"?) table.
+// - a member is tied to a single "User"
+// - a member is tied to an "Organization"
+// - a member has a set of permissions and
+
+// TODO: Define "Account" table.
+// - "Account" can also mean "Customer"
+// - an account belongs to an "Organization"
+// - keep track of "activity"; i.e., comments that members of the org can post
+// - an account can have many "Opportunities"
+
+// TODO: Define "Opportunity" table.
+// - an opportunity is tied to a single "Account"
+// - can be commented on by "Members"
+// - can be assigned to a "Member"
 
 export default defineDb({
   tables: {
