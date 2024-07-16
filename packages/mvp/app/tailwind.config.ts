70bb6d5a6a9e556b6f431a7c52e20d3c33054121
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-07-16 09:58:24 -0400

    build(css): use Pico CSS for base styles

--- packages/mvp/app/tailwind.config.ts
+++ packages/mvp/app/tailwind.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from "tailwindcss";
-import forms from "@tailwindcss/forms";
 
 const config: Config = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  plugins: [
-    forms(),
-  ]
+  corePlugins: {
+    // Use Pico CSS for base styles.
+    preflight: false,
+  },
 };
 
 export default config;
