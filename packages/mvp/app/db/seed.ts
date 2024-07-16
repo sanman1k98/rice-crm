6f390de2dfc36f9aae119088983f60425c996be9
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-07-16 11:15:36 -0400

    feat(db): seed tasks for "test-org"

--- packages/mvp/app/db/seed.ts
+++ packages/mvp/app/db/seed.ts
@@ -16,15 +16,38 @@ function devOrg(userId: string, name: string): typeof Organization.$inferInsert 
   };
 }
 
+function devTask(
+  orgId: string,
+  creatorId: string,
+  tasks: Omit<typeof Task.$inferInsert, "org_id" | "created_by" >[],
+): typeof Task.$inferInsert[] {
+  return tasks.map(t => ({
+    org_id: orgId,
+    created_by: creatorId,
+    ...t,
+  }));
+}
+
 export default async function() {
   const users = await Promise.all([
     devUser("test", "password"),
   ]);
 
+  const testOrgCreator = users[0]!.id!;
+
   const orgs = [
-    devOrg(users[0]!.id!, "test-org")
+    devOrg(testOrgCreator, "test-org")
   ];
+
+  const testOrg = orgs[0]!;
+
+  const testOrgTasks = devTask(testOrg.id!, testOrg.created_by!, [
+    { number: 1, title: "Create backend" },
+    { number: 2, title: "Create frontend" },
+    { number: 3, title: "Profit" },
+  ]);
 
   await db.insert(User).values(users);
   await db.insert(Organization).values(orgs)
+  await db.insert(Task).values(testOrgTasks)
 }
