af1dbfb9a0eef15de168b102989fcb3c2a895a07
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-07-16 11:00:15 -0400

    feat(db): seed "test-org"

--- packages/mvp/app/db/seed.ts
+++ packages/mvp/app/db/seed.ts
@@ -1,4 +1,4 @@
-import { db, User } from "astro:db";
+import { db, User, Organization, Task } from "astro:db";
 import { generateId, scrypt } from "@/auth";
 
 async function devUser(name: string, password: string): Promise<typeof User.$inferInsert> {
@@ -9,10 +9,22 @@ async function devUser(name: string, password: string): Promise<typeof User.$inf
   }
 }
 
+function devOrg(userId: string, name: string): typeof Organization.$inferInsert {
+  return {
+    id: name,
+    created_by: userId,
+  };
+}
+
 export default async function() {
   const users = await Promise.all([
     devUser("test", "password"),
   ]);
 
+  const orgs = [
+    devOrg(users[0]!.id!, "test-org")
+  ];
+
   await db.insert(User).values(users);
+  await db.insert(Organization).values(orgs)
 }
