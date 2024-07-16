44d4f5c7b29b0ab44ba772b1b7a68a6a2a3550bf
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-07-16 10:32:24 -0400

    feat(api): implement logout endpoint

--- packages/mvp/app/src/pages/api/v0/auth/logout.ts
+++ packages/mvp/app/src/pages/api/v0/auth/logout.ts
@@ -1,6 +1,22 @@
 import type { APIRoute } from "astro";
+import { lucia } from "@/auth";
 
-export const POST: APIRoute = async () => {
-  const data = { status: 501, statusText: "Not Implemented" };
-  return Response.json(data, data);
+/**
+ * Adapted from Lucia's documentation.
+ *
+ * @see https://lucia-auth.com/tutorials/username-and-password/astro
+ */
+export const POST: APIRoute = async (context) => {
+  if (!context.locals.session) {
+    return new Response(null, {
+      status: 401
+    });
+  }
+
+  await lucia.invalidateSession(context.locals.session.id);
+
+  const sessionCookie = lucia.createBlankSessionCookie();
+  context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+
+  return context.redirect("/");
 }
