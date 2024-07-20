347cd1a598a66be3b80e0f1c3a2875da55757729
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-07-20 11:43:49 -0400

    refactor: move utils to new "lib" dir

--- packages/mvp/app/src/lib/orgs.ts
+++ packages/mvp/app/src/lib/orgs.ts
@@ -1,25 +1,31 @@
 /**
- * @file Prepared SQL statements.
+ * @file Functions to interact with orgs using prepared SQL statements.
  */
 import { Account, Organization, OrgRole, Task, User, db, eq, sql } from "astro:db";
 
-export const orgInfo = db
+const orgInfo = db
   .select()
   .from(Organization)
-  .where(eq(sql.placeholder("org"), Organization.id))
+  .where(eq(sql.placeholder("id"), Organization.id))
   .prepare();
+
+export const getOrgInfo = (id: string) => orgInfo.get({ id });
 
 export const orgTasks = db
   .select()
   .from(Task)
-  .where(eq(Task.org, sql.placeholder("org")))
+  .where(eq(Task.org, sql.placeholder("id")))
   .prepare();
 
-export const orgAccounts = db
+export const getOrgTasks = (id: string) => orgTasks.all({ id });
+
+const orgAccounts = db
   .select()
   .from(Account)
-  .where(eq(Account.org, sql.placeholder("org")))
+  .where(eq(Account.org, sql.placeholder("id")))
   .prepare();
+
+export const getOrgAccounts = (id: string) => orgAccounts.all({ id });
 
 export const orgMembers = db
   .select()
