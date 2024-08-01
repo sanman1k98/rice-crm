ca3a84acce4ac0f50538734db80bc5df278fac45
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-08-01 13:22:38 -0400

    refactor(lib): docstrings and other changes
    Update `lib/accounts.ts` to be more consistent with the other lib
    modules. Use prepared statement for `getAccountInfo()` function.
    Annotate return types for exported functions.

--- packages/mvp/app/src/lib/accounts.ts
+++ packages/mvp/app/src/lib/accounts.ts
@@ -1,19 +1,34 @@
 import { Account, db, eq, sql } from "astro:db";
 
-export async function createAccount(
-  account: typeof Account.$inferInsert,
-) {
+type AccountId = typeof Account.$inferSelect["id"];
+type AccountInit = Omit<typeof Account.$inferInsert, "id">;
+type AccountInfo = typeof Account.$inferSelect;
+
+/**
+ * Create a new account.
+ *
+ * @param opts - Properties describing the new account.
+ * @returns The row inserted into the `Account` table.
+ */
+export async function createAccount(opts: AccountInit): Promise<AccountInfo> {
   return db
     .insert(Account)
-    .values(account)
+    .values(opts)
     .returning()
     .get();
 }
 
-export async function getAccountInfo(accountId: typeof Account.$inferSelect["id"]) {
-  return db
-    .select()
-    .from(Account)
-    .where(eq(sql.placeholder("id"), Account.id))
-    .get({ id: accountId });
-}
+const selectAccount = db
+  .select()
+  .from(Account)
+  .where(eq(Account.id, sql.placeholder("id")))
+  .prepare();
+
+/**
+ * Get information about an account.
+ *
+ * @param id - The `id` of the account.
+ * @returns The first matching row from the `Account` table if found.
+ */
+export const getAccountInfo = (id: AccountId): Promise<AccountInfo | undefined> =>
+  selectAccount.get({ id });
