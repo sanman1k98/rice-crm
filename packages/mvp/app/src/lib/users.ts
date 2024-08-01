8341b5be433c5422a424bc94e2ef1cec5a954900
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-08-01 12:05:02 -0400

    refactor(lib): docstrings and modify exports
    Sizeable refactor of "lib/users.ts" module:
    - Add long ass comment about inaccurate types and implicit runtime
    behaviors in Astro DB.
    - Omit "password_hash" column when returning user info.
    - Create and use some small utility types.
    - Export "ROLES".
    - Make it more consistent with other lib modules.

--- packages/mvp/app/src/lib/users.ts
+++ packages/mvp/app/src/lib/users.ts
@@ -1,45 +1,112 @@
 import { OrgRole, User, db, eq, sql } from "astro:db";
 import { generateId, scrypt } from "@/auth";
 
+export const ROLES = [
+  "member",
+  "owner",
+] as const;
+
+type UserId = typeof User.$inferSelect["id"];
+type OmitPasswordHash<T> = Omit<T, "password_hash">;
+type UserInfo = OmitPasswordHash<typeof User.$inferSelect>;
+
 type CreateUserValue = Omit<typeof User.$inferInsert, "id" | "password_hash"> & {
   password: string;
-  role?: "member" | "owner";
+  /** @defaultValue `"member"`*/
+  role?: typeof ROLES[number];
 };
 
-export const createUser = async (user: CreateUserValue) => {
+/**
+ * WARN: `@astrojs/db@0.11.7` table types DO NOT accurately match their runtime values.
+ *
+ * Astro DB uses Drizzle ORM under the hood but with some added functionality
+ * in order to simplify the developer experience. For example, you can define a
+ * table without a column to use as the primary key and Astro DB will
+ * implicitly create one for you. Unfortunately, this behavior appears to be
+ * undocumented and the generated types don't seem to reflect this.
+ *
+ * We should use `getTableColumns()` from Drizzle which can be used to omit
+ * certain columns for a partial selection, but Astro DB does not re-export
+ * that helper function.
+ *
+ * @see https://orm.drizzle.team/docs/goodies#get-typed-table-columns
+ *
+ * According to the generated type information for `User`, we should be able to
+ * use object destructuring and the spread operator to create the
+ * `partialUserColumns` object below.
+ *
+ * ```ts
+ * const { password_hash: _, ...partialUserColumns } = User._.columns;
+ * ```
+ *
+ * But the generated type information intended for the developer using the ORM
+ * do not include all of the private runtime internals that power its full
+ * type-safety, hence the reason for the `getTableColumns()` helper function.
+ *
+ * At least we can use the generated types to derive our own type which we can
+ * then use to constrain the `partialUserColumns` object.
+ */
+
+const partialUserColumns: OmitPasswordHash<typeof User._.columns> = {
+  id: User.id,
+  username: User.username,
+  fullname: User.fullname,
+  primary_org: User.primary_org,
+};
+
+/**
+ * Create a new user and define their role within the given organization.
+ *
+ * @param user -
+ * An object with properties defining the new user, including an
+ * optional `role` property to change from default.
+ *
+ * @returns
+ * The row inserted into the `User` table including the generated `id`, but
+ * without the `password_hash` column.
+ */
+export async function createUser(user: CreateUserValue): Promise<UserInfo> {
   const {
-    primary_org: orgId,
     password,
     role = "member",
     ...rest
   } = user;
-  const userId = generateId();
+
+  // Insert the new user and get it back.
   const newUser = await db
     .insert(User)
     .values({
-      id: userId,
-      primary_org: orgId,
+      id: generateId(),
       password_hash: await scrypt.hash(password),
       ...rest,
     })
-    .returning()
+    .returning(partialUserColumns)
     .get();
+
+  // Define their role in their primary org.
   await db
     .insert(OrgRole)
     .values({
-      org: orgId,
-      user: userId,
+      user: newUser.id,
+      org: newUser.primary_org,
       role,
     });
+
+  // TODO: Return both `UserInfo` and `OrgRole` information.
   return newUser;
 };
 
 const selectUser = db
-  .select()
+  .select(partialUserColumns)
   .from(User)
   .where(eq(sql.placeholder("id"), User.id))
   .prepare();
 
-export async function getUserInfo(id: typeof User.$inferSelect["id"]) {
-  return selectUser.get({ id });
-}
+/**
+ * Get information about the given user.
+ *
+ * @param id - The `id` of the user.
+ * @returns The first matching row from the `User` table if found.
+ */
+export const getUserInfo = (id: UserId): Promise<UserInfo | undefined> =>
+  selectUser.get({ id }); 
