76b47c21f5be565d34b0815271742045f6d35e93
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-08-01 13:40:27 -0400

    refactor(lib): docstrings and func to get info
    Refactor `lib/opportunites.ts` module to be more consistent with the
    other lib modules.

--- packages/mvp/app/src/lib/opportunities.ts
+++ packages/mvp/app/src/lib/opportunities.ts
@@ -1,4 +1,7 @@
-import { db, Opportunity } from "astro:db";
+/**
+ * @file CRUD operations for opportunities.
+ */
+import { Opportunity, db, eq, sql } from "astro:db";
 import { z } from "astro/zod";
 
 export const STAGES = [
@@ -9,19 +12,44 @@ export const STAGES = [
 
 const stagesSchema = z.enum(STAGES);
 
-export async function createOpportunity(
-  opportunity: typeof Opportunity.$inferInsert & { stage: typeof STAGES[number] },
-) {
-  const parsed = stagesSchema.safeParse(opportunity.stage);
+type OpportunityId = typeof Opportunity.$inferSelect["id"];
+type OpportunityInfo = typeof Opportunity.$inferSelect;
+type OpportunityInit = Omit<typeof Opportunity.$inferInsert, "id"> & {
+  stage: typeof STAGES[number];
+};
+
+/**
+ * Create a new opportunity.
+ *
+ * @param opts - Properties describing the opportunity.
+ * @returns The row inserted into the `Opportunity` table.
+ */
+export async function createOpportunity(opts: OpportunityInit): Promise<OpportunityInfo> {
+  const parsed = stagesSchema.safeParse(opts.stage);
   if (!parsed.success && parsed.error) {
     throw new Error(
-      `Invalid stage "${opportunity.stage}" when creating opportunity "${opportunity.name}"`,
+      `Invalid stage "${opts.stage}" when creating opportunity "${opts.name}"`,
       { cause: parsed.error }
     );
   }
   return db
     .insert(Opportunity)
-    .values(opportunity)
+    .values(opts)
     .returning()
     .get();
 }
+
+const selectOpportunity = db
+  .select()
+  .from(Opportunity)
+  .where(eq(Opportunity.id, sql.placeholder("id")))
+  .prepare();
+
+/**
+ * Get information about an opportunity.
+ *
+ * @param id - The `id` of the opportunity.
+ * @returns The first matching row from the `Opportunity` table if found.
+ */
+export const getOpportunityInfo = (id: OpportunityId): Promise<OpportunityInfo | undefined> =>
+  selectOpportunity.get({ id });
