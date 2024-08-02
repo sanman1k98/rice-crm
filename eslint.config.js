5428b66138f3d8f44d53e42d2f1af357243c00c3
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-08-02 15:14:47 -0400

    build(lint): configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import js from "@eslint/js";
+import ts, { config as defineConfig } from "typescript-eslint";
+
+const NAME_PREFIX = "user";
+
+/**
+ * @param {...string} scopes 
+ * @returns {string} 
+ */
+function createRuleName(...scopes) {
+  return [NAME_PREFIX, ...scopes].join("/");
+}
+
+export default defineConfig(
+  {
+    // Global ignores
+    // @see https://eslint.org/docs/latest/use/configure/configuration-files#globally-ignoring-files-with-ignores
+    name: createRuleName("ignores"),
+    ignores: ["**/dist/", "**/.astro/"],
+  },
+  {
+    name: "eslint/js/recommended",
+    ...js.configs.recommended,
+  },
+  ...ts.configs.strict,
+  ...ts.configs.strictTypeChecked,
+  {
+    name: createRuleName("ts", "lang"),
+    languageOptions: {
+      parserOptions: {
+        tsconfigRootDir: import.meta.dirname,
+        project: [
+          "./tsconfig.eslint.json",
+          "./packages/mvp/app/tsconfig.json",
+        ],
+      },
+    },
+  },
+  {
+    name: createRuleName("ts"),
+    rules: {
+      "@typescript-eslint/triple-slash-reference": "off",
+    },
+  },
+);
