87c58cdab676363621c69a56692c3a6be1a32e80
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-09-09 15:57:38 -0400

    chore(api): func to create service DB client

---
+++ packages/mvp/api/src/db/clients/service.ts
@@ -0,0 +1,16 @@
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client'
+import * as schema from '@/db/schemas/service';
+import { localServiceDbPath } from '../../../db-shared/utils';
+
+export function createServiceDbClient() {
+  let url: string, authToken: string | undefined; 
+  if (import.meta.env.PROD) {
+    url = import.meta.env.SERVICE_DB_URL!;
+    authToken = import.meta.env.SERVICE_DB_AUTH_TOKEN!;
+  } else {
+    url = localServiceDbPath();
+    authToken = undefined;
+  }
+   return drizzle(createClient({ url, authToken }), { schema });
+}
