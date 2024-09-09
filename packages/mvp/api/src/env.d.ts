dfce44f82231ed55d715500b83ffab6c1c189039
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-09-09 15:33:45 -0400

    chore(api): add vite types

---
+++ packages/mvp/api/src/env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly SERVICE_DB_URL?: string
+  readonly SERVICE_DB_AUTH_TOKEN?: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
