b925d9b0da117ac9ddcff475e16c4b1aa65b2fe9
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-09-09 15:58:36 -0400

    feat(api): route to retrive a list of orgs

--- packages/mvp/api/src/index.ts
+++ packages/mvp/api/src/index.ts
@@ -1,7 +1,15 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { organizations } from '@/db/schemas/service';
+import { createServiceDbClient } from '@/db/clients/service';
 
 const api = new Hono()
 
 api.get('/', (c) => c.text('Hello CRM!'))
+
+api.get('/orgs', async (c) => {
+  const db = createServiceDbClient();
+  const orgs = await db.select().from(organizations).all();
+  return c.json(orgs);
+})
 
 export default api
