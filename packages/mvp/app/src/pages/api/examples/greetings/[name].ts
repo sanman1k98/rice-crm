5ebe8319dfa5a36c302d75980f4acbbc45277c7e
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-09-11 10:14:09 -0400

    chore(app): remove example endpoints

--- packages/mvp/app/src/pages/api/examples/greetings/[name].ts
+++
@@ -1,6 +0,0 @@
-import type { APIRoute } from "astro";
-
-const capitalize = (str: string) => str.charAt(0).toUpperCase() + str.slice(1);
-
-export const GET: APIRoute<never, { name: string }> = ({ params }) =>
-  Response.json({ text: `Hello ${capitalize(params.name)}!` });
--- packages/mvp/app/src/pages/api/examples/greetings/index.ts
+++
@@ -1,3 +0,0 @@
-import type { APIRoute } from "astro";
-
-export const GET: APIRoute = () => Response.json({ text: "Hello World!" });
