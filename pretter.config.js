d9f3acc3054ca1c5011c55fb91b8737332d33489
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-09-16 08:38:49 -0400

    style: use prettier

---
+++ pretter.config.js
@@ -0,0 +1,33 @@
+// Adapted from https://github.com/withastro/astro
+/** @type {import("prettier").Config} */
+export default {
+	printWidth: 100,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	useTabs: true,
+	trailingComma: 'all',
+	// Matches eslint `stylistic/arrow-paren` rule.
+	arrowParens: 'always',
+	plugins: [
+		'prettier-plugin-astro',
+		'prettier-plugin-tailwindcss',
+	],
+	overrides: [
+		{
+			files: ['.*', '*.md', '*.toml', '*.yml'],
+			options: {
+				useTabs: false,
+			},
+		},
+		{
+			files: ['**/*.astro'],
+			options: {
+				parser: 'astro',
+				astroAllowShorthand: true,
+				// Use eslint to lint and format frontmatter.
+				astroSkipFrontmatter: true,
+			},
+		},
+	],
+};
