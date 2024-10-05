3ce91b67168c35daeecc904e12b46caba3443b72
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-10-05 09:38:14 -0400

    impr(lib): zod schemas for shared types

--- packages/mvp/app/src/lib/shared.ts
+++ packages/mvp/app/src/lib/shared.ts
@@ -1,12 +1,24 @@
+import { z } from 'astro:schema';
+
 export interface EmailInfo {
 	label: string;
 	email: string;
 };
 
+export const EmailInfoSchema = z.strictObject({
+	label: z.string(),
+	email: z.string().email(),
+}) satisfies z.ZodType<EmailInfo>;
+
 export interface PhoneInfo {
 	label: string;
 	phone: string;
 };
+
+export const PhoneInfoSchema = z.strictObject({
+	label: z.string(),
+	phone: z.string(),
+}) satisfies z.ZodType<PhoneInfo>;
 
 export interface AddressInfo {
 	label: string;
@@ -17,8 +29,22 @@ export interface AddressInfo {
 	zip: number;
 };
 
+export const AddressInfoSchema = z.strictObject({
+	label: z.string(),
+	street1: z.string(),
+	street2: z.string(),
+	city: z.string(),
+	state: z.string(),
+	zip: z.number(),
+}) satisfies z.ZodType<AddressInfo>;
+
 export interface LinkInfo {
 	label: string;
 	/** A webpage URL. */
 	link: string;
 };
+
+export const LinkInfoSchema = z.strictObject({
+	label: z.string(),
+	link: z.string().url(),
+}) satisfies z.ZodType<LinkInfo>;
