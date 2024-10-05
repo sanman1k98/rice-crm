7d00d07f46ee3a250133340d159b4e5190c9a7c1
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-10-05 09:46:33 -0400

    refactor(lib): derive types from schemas

--- packages/mvp/app/src/lib/shared.ts
+++ packages/mvp/app/src/lib/shared.ts
@@ -1,33 +1,14 @@
 import { z } from 'astro:schema';
 
-export interface EmailInfo {
-	label: string;
-	email: string;
-};
-
 export const EmailInfoSchema = z.strictObject({
 	label: z.string(),
 	email: z.string().email(),
-}) satisfies z.ZodType<EmailInfo>;
-
-export interface PhoneInfo {
-	label: string;
-	phone: string;
-};
+});
 
 export const PhoneInfoSchema = z.strictObject({
 	label: z.string(),
 	phone: z.string(),
-}) satisfies z.ZodType<PhoneInfo>;
-
-export interface AddressInfo {
-	label: string;
-	street1: string;
-	street2: string;
-	city: string;
-	state: string;
-	zip: number;
-};
+});
 
 export const AddressInfoSchema = z.strictObject({
 	label: z.string(),
@@ -36,15 +17,14 @@ export const AddressInfoSchema = z.strictObject({
 	city: z.string(),
 	state: z.string(),
 	zip: z.number(),
-}) satisfies z.ZodType<AddressInfo>;
-
-export interface LinkInfo {
-	label: string;
-	/** A webpage URL. */
-	link: string;
-};
+});
 
 export const LinkInfoSchema = z.strictObject({
 	label: z.string(),
 	link: z.string().url(),
-}) satisfies z.ZodType<LinkInfo>;
+});
+
+export type EmailInfo = z.infer<typeof EmailInfoSchema>;
+export type PhoneInfo = z.infer<typeof PhoneInfoSchema>;
+export type AddressInfo = z.infer<typeof AddressInfoSchema>;
+export type LinkInfo = z.infer<typeof LinkInfoSchema>;
