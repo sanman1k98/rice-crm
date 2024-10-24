2a96bf3733528606f309e46d05aa90c6a9b4bffe
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-10-23 22:21:54 -0400

    chore: seed deals

--- packages/mvp/app/db/seed.ts
+++ packages/mvp/app/db/seed.ts
@@ -7,13 +7,14 @@
  */
 import type { CompanyId, CompanyInit } from '@/lib/companies';
 import type { ContactId, ContactInit } from '@/lib/contacts';
-import type { LeadInit } from '@/lib/leads';
+import type { DealInit } from '@/lib/deals';
+import type { LeadId, LeadInit } from '@/lib/leads';
 import { createAccount } from '@/lib/accounts';
 import { createOpportunity, OpportunityStageEnum } from '@/lib/opportunities';
 import { createTasks } from '@/lib/tasks';
 import { createUser, OrgRoleValueEnum, type UserId, type UserInfo } from '@/lib/users';
 import { faker } from '@faker-js/faker';
-import { Company, Contact, db, Lead } from 'astro:db';
+import { Company, Contact, db, Deal, Lead } from 'astro:db';
 
 async function seedDeprecatedTables(user: UserInfo) {
 	const testCustomer = await createAccount({
@@ -87,6 +88,15 @@ function generateLead(opts: { author: UserId; contact: ContactId }) {
 	} satisfies LeadInit;
 }
 
+function generateDeal(opts: { lead: LeadId; company?: CompanyId }) {
+	const dealInit = {
+		company: opts.company ?? null,
+		amount: faker.number.float({ min: 1000, max: 20000, multipleOf: 100 }),
+		currency: 'USD',
+	} satisfies DealInit;
+	return { lead: opts.lead, ...dealInit };
+}
+
 export default async function () {
 	const testUser = await createUser({
 		fullname: 'Firstname Lastname',
@@ -139,11 +149,21 @@ export default async function () {
 		.groupBy(Contact.company)
 		.all();
 
-	await db
+	const leads = await db
 		.insert(Lead)
 		.values(
 			contactPerCompany.map(
 				({ id }) => generateLead({ contact: id, author: testUser.id }),
 			),
+		)
+		.returning({ id: Lead.id })
+		.all();
+
+	await db
+		.insert(Deal)
+		.values(
+			leads
+				.filter((_lead, idx) => (idx & 1) === 1)
+				.map((lead) => generateDeal({ lead: lead.id })),
 		);
 }
