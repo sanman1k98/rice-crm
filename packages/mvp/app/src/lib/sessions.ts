b1e4fe254f430e2f91599dd9ff97074be084a3f1
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2024-10-26 12:40:55 -0400

    chore(lib): implement basic sessions API (#5)
    Copypasta from Lucia's docs. I really should go back at some point and
    try to understand everything going on and add a bunch of comments to
    this code.

---
+++ packages/mvp/app/src/lib/sessions.ts
@@ -0,0 +1,68 @@
+/**
+ * @file Adapted from Lucia's docs: Sessions with Drizzle ORM.
+ * @see https://lucia-auth.com/sessions/basic-api/drizzle-orm
+ */
+import { sha256 } from '@oslojs/crypto/sha2';
+import { encodeBase32LowerCaseNoPadding, encodeHexLowerCase } from '@oslojs/encoding';
+import { db, eq, Session, User } from 'astro:db';
+
+type TUser = typeof User.$inferSelect;
+type TSession = typeof Session.$inferSelect;
+
+export type SessionValidationResult =
+	| { session: TSession; user: TUser }
+	| { session: null; user: null };
+
+export function generateSessionToken(): string {
+	const bytes = new Uint8Array(20);
+	crypto.getRandomValues(bytes);
+	const token = encodeBase32LowerCaseNoPadding(bytes);
+	return token;
+}
+
+export async function createSession(token: string, userId: TUser['id']): Promise<TSession> {
+	const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+	const session: TSession = {
+		id: sessionId,
+		userId,
+		expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+	};
+	await db.insert(Session).values(session);
+	return session;
+}
+
+export async function validateSessionToken(token: string): Promise<SessionValidationResult> {
+	const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+	const result = await db
+		.select({ user: User, session: Session })
+		.from(Session)
+		.innerJoin(User, eq(Session.userId, User.id))
+		.where(eq(Session.id, sessionId))
+		.get();
+
+	if (!result)
+		return { session: null, user: null };
+
+	const { user, session } = result;
+
+	if (Date.now() >= session.expiresAt.getTime()) {
+		await db.delete(Session).where(eq(Session.id, session.id));
+		return { session: null, user: null };
+	}
+
+	if (Date.now() >= session.expiresAt.getTime() - 1000 * 60 * 60 * 24 * 15) {
+		session.expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+		await db
+			.update(Session)
+			.set({
+				expiresAt: session.expiresAt,
+			})
+			.where(eq(Session.id, session.id));
+	}
+
+	return { session, user };
+}
+
+export async function invalidateSession(sessionId: TSession['id']): Promise<void> {
+	await db.delete(Session).where(eq(Session.id, sessionId));
+}
